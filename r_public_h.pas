bf97b3f552c93890e5b661df26e3661eed98c968
jval1972 <jvalavanis@gmail.com>
2020-06-12 18:19:43 +0300

    Formatting work

--- r_public_h.pas
+++ r_public_h.pas
@@ -1,284 +1,282 @@
+(***************************************************************************)
+(*                                                                         *)
+(* xGreed - Source port of the game "In Pursuit of Greed"                  *)
+(* Copyright (C) 2020 by Jim Valavanis                                     *)
+(*                                                                         *)
+(* Raven 3D Engine                                                         *)
+(* Copyright (C) 1996 by Softdisk Publishing                               *)
+(*                                                                         *)
+(* Original Design:                                                        *)
+(*  John Carmack of id Software                                            *)
+(*                                                                         *)
+(* Enhancements by:                                                        *)
+(*  Robert Morgan of Channel 7............................Main Engine Code *)
+(*  Todd Lewis of Softdisk Publishing......Tools,Utilities,Special Effects *)
+(*  John Bianca of Softdisk Publishing..............Low-level Optimization *)
+(*  Carlos Hasan..........................................Music/Sound Code *)
+(*                                                                         *)
+(*                                                                         *)
+(***************************************************************************)
 
 unit r_public_h;
+
 interface
 
-{
-  Automatically converted by H2Pas 1.0.0 from R_public.h
-  The following command line parameters were used:
-    -o
-    r_public_h.pas
-    R_public.h
-}
-
-    Type
-    Pdoorobj_t  = ^doorobj_t;
-    Pelevobj_t  = ^elevobj_t;
-    Pscaleobj_t  = ^scaleobj_t;
-    Pspawnarea_t  = ^spawnarea_t;
 {$IFDEF FPC}
 {$PACKRECORDS C}
 {$ENDIF}
 
 
-  {************************************************************************* }
-  {                                                                          }
-  {                                                                          }
-  { Raven 3D Engine                                                          }
-  { Copyright (C) 1995 by Softdisk Publishing                                }
-  {                                                                          }
-  { Original Design:                                                         }
-  {  John Carmack of id Software                                             }
-  {                                                                          }
-  { Enhancements by:                                                         }
-  {  Robert Morgan of Channel 7............................Main Engine Code  }
-  {  Todd Lewis of Softdisk Publishing......Tools,Utilities,Special Effects  }
-  {  John Bianca of Softdisk Publishing..............Low-level Optimization  }
-  {  Carlos Hasan..........................................Music/Sound Code  }
-  {                                                                          }
-  {                                                                          }
-  {************************************************************************* }
-{$ifndef R_PUBLIC_H}
-{$define R_PUBLIC_H}  
-  {*** CONSTANTS *** }
+{*** CONSTANTS *** }
 
-  const
-    MAX_VIEW_WIDTH = 320;    
-    MAX_VIEW_HEIGHT = 200;    
-    INIT_VIEW_WIDTH = 320;    
-    INIT_VIEW_HEIGHT = 200;    
-    VIEW_LEFT = 0;    
-    VIEW_TOP = 0;    
+const
+  MAX_VIEW_WIDTH = 320;
+  MAX_VIEW_HEIGHT = 200;
+  INIT_VIEW_WIDTH = 320;
+  INIT_VIEW_HEIGHT = 200;
+  VIEW_LEFT = 0;
+  VIEW_TOP = 0;
 
-  type
-    fixed_t = longint;
+type
+  fixed_t = longint;
 
-  const
-    WALL_CONTACT = 1;    
-    DOOR_CONTACT = 2;    
-    FRACBITS = 16;    
-    FRACUNIT = 1 shl FRACBITS;    
-    TILEUNIT = 64*FRACUNIT;    
-    HALFTILEUNIT = 32*FRACUNIT;    
-    PI = 3.14159265;    
-    MAPSIZE = 64;    { there must not be any 65th vertexes }
-    MAPROWS = 64;    
-    MAPCOLS = 64;    
-    TILESHIFT = 6;    
-    TILESIZE = 1 shl TILESHIFT;    { pixels to tile }
-    TILEFRACSHIFT = TILESHIFT+FRACBITS;    
-    TILEGLOBAL = 1 shl TILEFRACSHIFT;    
-    ANGLES = 1023;    
-    WEST = 512;    
-    EAST = 0;    
-    NORTH = 256;    
-    SOUTH = 768;    
-    DEGREE45 = 128;    
-    DEGREE45_2 = 64;    
-    MINZ = FRACUNIT/2;    
-  {first value is the maximum # of tiles to render outwards }
-    MAXZ = (32 shl (FRACBITS+TILESHIFT))-1;    
-    MAXZLIGHT = MAXZ shr FRACBITS;    
-    MAXDOORS = 32;    
-    MAXSPRITES = 700;    
-    MAXELEVATORS = 128;    
-    MAXSPAWNAREAS = 96;    
-    ANIM_LOOP_MASK = 1;    
-    ANIM_CG_MASK = 30;    
-    ANIM_MG_MASK = 480;    
-    ANIM_SELFDEST = 32768;    
-    ANIM_DELAY_MASK = 32256;    
-(* error 
-#define MINDIST         (FRACUNIT*12)
-in define line 71 *)
-      PLAYERSIZE = 16 shl FRACBITS;      
-      FRACTILESHIFT = FRACBITS+TILESHIFT;      
-      BACKDROPHEIGHT = 100;      
-      MAXSCROLL = 60;      
-      MAXSCROLL2 = 120;      
-    { flags in mapflags }
-      FL_DOOR = 128;      
-      FL_FLOOR = 7;      
-      FL_CEILING = 56;      
-      FL_AUX = 64;      { not used right now }
-      FLS_FLOOR = 0;      
-      FLS_CEILING = 3;      
-      POLY_FLAT = 0;      
-      POLY_SLOPE = 1;      
-      POLY_ULTOLR = 2;      
-      POLY_URTOLL = 3;      
-    { additional POLY_??? can be defined from 4-7 }
-      STEP_COLOR = 132;      
-      WALL_COLOR = 127;      
-      TRANS_COLOR = 79;      
-      DOOR_COLOR = 73;      
-    {*** TYPES *** }
+const
+  WALL_CONTACT = 1;
+  DOOR_CONTACT = 2;
+  FRACBITS = 16;
+  FRACUNIT = 1 shl FRACBITS;
+  TILEUNIT = 64*FRACUNIT;
+  HALFTILEUNIT = 32*FRACUNIT;
+  PI = 3.14159265;
+  MAPSIZE = 64;    { there must not be any 65th vertexes }
+  MAPROWS = 64;
+  MAPCOLS = 64;
+  TILESHIFT = 6;
+  TILESIZE = 1 shl TILESHIFT;    { pixels to tile }
+  TILEFRACSHIFT = TILESHIFT+FRACBITS;
+  TILEGLOBAL = 1 shl TILEFRACSHIFT;
+  ANGLES = 1023;
+  WEST = 512;
+  EAST = 0;
+  NORTH = 256;
+  SOUTH = 768;
+  DEGREE45 = 128;
+  DEGREE45_2 = 64;
+  MINZ = FRACUNIT/2;
+  // first value is the maximum # of tiles to render outwards
+  MAXZ = (32 shl (FRACBITS + TILESHIFT)) - 1;
+  MAXZLIGHT = MAXZ shr FRACBITS;
+  MAXDOORS = 32;
+  MAXSPRITES = 700;
+  MAXELEVATORS = 128;
+  MAXSPAWNAREAS = 96;
+  ANIM_LOOP_MASK = 1;
+  ANIM_CG_MASK = 30;
+  ANIM_MG_MASK = 480;
+  ANIM_SELFDEST = 32768;
+  ANIM_DELAY_MASK = 32256;
+  MINDIST = FRACUNIT * 12;
+  PLAYERSIZE = 16 shl FRACBITS;
+  FRACTILESHIFT = FRACBITS+TILESHIFT;
+  BACKDROPHEIGHT = 100;
+  MAXSCROLL = 60;
+  MAXSCROLL2 = 120;
+{ flags in mapflags }
+  FL_DOOR = 128;
+  FL_FLOOR = 7;
+  FL_CEILING = 56;
+  FL_AUX = 64;      { not used right now }
+  FLS_FLOOR = 0;
+  FLS_CEILING = 3;
+  POLY_FLAT = 0;
+  POLY_SLOPE = 1;
+  POLY_ULTOLR = 2;
+  POLY_URTOLL = 3;
+{ additional POLY_??? can be defined from 4-7 }
+  STEP_COLOR = 132;
+  WALL_COLOR = 127;
+  TRANS_COLOR = 79;
+  DOOR_COLOR = 73;
+{*** TYPES *** }
 
-    type
-      pixel_t = byte;
+type
+  pixel_t = byte;
 
-      rotate_t = (rt_one,rt_four,rt_eight);
+  rotate_t = (rt_one, rt_four, rt_eight);
 
-      special_t = (st_none,st_noclip,st_transparent,st_maxlight
-        );
+  special_t = (st_none, st_noclip, st_transparent, st_maxlight);
 
-      orientation_t = (dr_horizontal,dr_vertical,dr_horizontal2,
-        dr_vertical2);
+  orientation_t = (dr_horizontal, dr_vertical, dr_horizontal2, dr_vertical2);
 
-      elevtype = (E_NORMAL,E_SWITCHDOWN,E_SWITCHDOWN2,
-        E_TIMED,E_SWITCHUP,E_SWAP,E_SECRET,E_TRIGGERED,
-        E_PRESSUREHIGH,E_PRESSURELOW);
-    { list links, don't touch }
-    { modify this part whenever you want }
-    { must accept all possible tick values }
-    { zadj = height above floor }
-    { global position of the BOTTOM of the shape }
-    { lumpnum is spritelump+basepic+rotation }
-    { 0 - ANGLES }
-    { how big he is }
-    { so it doesn't self destruct owner }
-    { id }
-    { who created it }
-    { vertical height }
-    { transparent, no clipping }
+  elevtype = (
+    E_NORMAL,
+    E_SWITCHDOWN,
+    E_SWITCHDOWN2,
+    E_TIMED,
+    E_SWITCHUP,
+    E_SWAP,
+    E_SECRET,
+    E_TRIGGERED,
+    E_PRESSUREHIGH,
+    E_PRESSURELOW
+  );
+{ list links, don't touch }
+{ modify this part whenever you want }
+{ must accept all possible tick values }
+{ zadj = height above floor }
+{ global position of the BOTTOM of the shape }
+{ lumpnum is spritelump+basepic+rotation }
+{ 0 - ANGLES }
+{ how big he is }
+{ so it doesn't self destruct owner }
+{ id }
+{ who created it }
+{ vertical height }
+{ transparent, no clipping }
 
-      scaleobj_s = record
-          prev: ^scaleobj_s;
-          next: ^scaleobj_s;
-          animation: longint;
-          animationTime: longint;
-          moveSpeed: fixed_t;
-          zadj: fixed_t;
-          x: fixed_t;
-          y: fixed_t;
-          z: fixed_t;
-          lastx: fixed_t;
-          lasty: fixed_t;
-          basepic: longint;
-          rotate: rotate_t;
-          angle: longint;
-          angle2: longint;
-          movesize: longint;
-          active: bool;
-          nofalling: bool;
-          intelligence: longint;
-          bullet: longint;
-          enraged: longint;
-          movetime: longint;
-          modetime: longint;
-          actiontime: longint;
-          scantime: longint;
-          firetime: longint;
-          heat: longint;
-          startpic: longint;
-          movemode: longint;
-          startspot: longint;
-          damage: longint;
-          hitpoints: longint;
-          _type: longint;
-          spawnid: longint;
-          score: longint;
-          maxmove: longint;
-          regen: longint;
-          deathevent: longint;
-          height: fixed_t;
-          specialtype: special_t;
-          scale: longint;
-        end;
-      scaleobj_t = scaleobj_s;
-    { modify this part whenever you want }
-    { probably only want to set this once }
-    { set true if the pic has any masked areas }
-    { lumpnum is doorlump+pic }
-    { should generally be set to the floor height }
-    { range from 0 (open) - FRACUNIT*64 (closed }
+  scaleobj_s = record
+    prev: ^scaleobj_s;
+    next: ^scaleobj_s;
+    animation: longint;
+    animationTime: longint;
+    moveSpeed: fixed_t;
+    zadj: fixed_t;
+    x: fixed_t;
+    y: fixed_t;
+    z: fixed_t;
+    lastx: fixed_t;
+    lasty: fixed_t;
+    basepic: longint;
+    rotate: rotate_t;
+    angle: longint;
+    angle2: longint;
+    movesize: longint;
+    active: bool;
+    nofalling: bool;
+    intelligence: longint;
+    bullet: longint;
+    enraged: longint;
+    movetime: longint;
+    modetime: longint;
+    actiontime: longint;
+    scantime: longint;
+    firetime: longint;
+    heat: longint;
+    startpic: longint;
+    movemode: longint;
+    startspot: longint;
+    damage: longint;
+    hitpoints: longint;
+    _type: longint;
+    spawnid: longint;
+    score: longint;
+    maxmove: longint;
+    regen: longint;
+    deathevent: longint;
+    height: fixed_t;
+    specialtype: special_t;
+    scale: longint;
+  end;
+  scaleobj_t = scaleobj_s;
+  Pscaleobj_t = ^scaleobj_t;
+{ modify this part whenever you want }
+{ probably only want to set this once }
+{ set true if the pic has any masked areas }
+{ lumpnum is doorlump+pic }
+{ should generally be set to the floor height }
+{ range from 0 (open) - FRACUNIT*64 (closed }
 
-      doorobj_s = record
-          tilex: longint;
-          tiley: longint;
-          doorOpen: bool;
-          doorOpening: bool;
-          doorClosing: bool;
-          doorBlocked: bool;
-          doorBumpable: bool;
-          doorSize: longint;
-          doorTimer: longint;
-          doorLocks: byte;
-          orientation: orientation_t;
-          transparent: bool;
-          pic: longint;
-          height: longint;
-          position: fixed_t;
-        end;
-      doorobj_t = doorobj_s;
-    { elevator structure }
-    { going up? }
-    { height }
-    { time for each movement }
-    { set to floorheight[mapspot] }
-    { set to ceilingheight[mapspot]-64 }
+  doorobj_s = record
+    tilex: longint;
+    tiley: longint;
+    doorOpen: bool;
+    doorOpening: bool;
+    doorClosing: bool;
+    doorBlocked: bool;
+    doorBumpable: bool;
+    doorSize: longint;
+    doorTimer: longint;
+    doorLocks: byte;
+    orientation: orientation_t;
+    transparent: bool;
+    pic: longint;
+    height: longint;
+    position: fixed_t;
+  end;
+  doorobj_t = doorobj_s;
+  Pdoorobj_t  = ^doorobj_t;
+{ elevator structure }
+{ going up? }
+{ height }
+{ time for each movement }
+{ set to floorheight[mapspot] }
+{ set to ceilingheight[mapspot]-64 }
 
-      elevobj_s = record
-          prev: ^elevobj_s;
-          next: ^elevobj_s;
-          elevUp: bool;
-          elevDown: bool;
-          position: longint;
-          elevTimer: longint;
-          floor: longint;
-          ceiling: longint;
-          mapspot: longint;
-          speed: longint;
-          eval: longint;
-          endeval: longint;
-          nosave: longint;
-          _type: elevtype;
-        end;
-      elevobj_t = elevobj_s;
+  elevobj_s = record
+    prev: ^elevobj_s;
+    next: ^elevobj_s;
+    elevUp: bool;
+    elevDown: bool;
+    position: longint;
+    elevTimer: longint;
+    floor: longint;
+    ceiling: longint;
+    mapspot: longint;
+    speed: longint;
+    eval: longint;
+    endeval: longint;
+    nosave: longint;
+    _type: elevtype;
+  end;
+  elevobj_t = elevobj_s;
+  Pelevobj_t  = ^elevobj_t;
 
-      spawnarea_s = record
-          mapspot: longint;
-          mapx: fixed_t;
-          mapy: fixed_t;
-          _type: longint;
-          time: longint;
-        end;
-      spawnarea_t = spawnarea_s;
-    {*** VARIABLES *** }
+  spawnarea_s = record
+      mapspot: longint;
+      mapx: fixed_t;
+      mapy: fixed_t;
+      _type: longint;
+      time: longint;
+    end;
+  spawnarea_t = spawnarea_s;
+  Pspawnarea_t  = ^spawnarea_t;
+{*** VARIABLES *** }
 
-      var
-        actionflag: longint;cvar;external;
-    { if set non 0, the action hook is called }
-        viewbuffer: array[0..(MAX_VIEW_WIDTH*MAX_VIEW_HEIGHT)-1] of pixel_t;cvar;external;
-        viewylookup: array[0..(MAX_VIEW_HEIGHT)-1] of ^pixel_t;cvar;external;
-        spritelump: longint;cvar;external;
-        numsprites: longint;cvar;external;
-        flattranslation: ^longint;cvar;external;
-        costable: array[0..(ANGLES+1)-1] of fixed_t;cvar;external;
-        westwall: array[0..(MAPROWS*MAPCOLS)-1] of byte;cvar;external;
-        westflags: array[0..(MAPROWS*MAPCOLS)-1] of byte;cvar;external;
-        northwall: array[0..(MAPROWS*MAPCOLS)-1] of byte;cvar;external;
-        northflags: array[0..(MAPROWS*MAPCOLS)-1] of byte;cvar;external;
-        floorpic: array[0..(MAPROWS*MAPCOLS)-1] of byte;cvar;external;
-        floorflags: array[0..(MAPROWS*MAPCOLS)-1] of byte;cvar;external;
-        ceilingpic: array[0..(MAPROWS*MAPCOLS)-1] of byte;cvar;external;
-        ceilingflags: array[0..(MAPROWS*MAPCOLS)-1] of byte;cvar;external;
-        floorheight: array[0..(MAPROWS*MAPCOLS)-1] of byte;cvar;external;
-        ceilingheight: array[0..(MAPROWS*MAPCOLS)-1] of byte;cvar;external;
-        floordef: array[0..(MAPROWS*MAPCOLS)-1] of byte;cvar;external;
-        floordefflags: array[0..(MAPROWS*MAPCOLS)-1] of byte;cvar;external;
-        ceilingdef: array[0..(MAPROWS*MAPCOLS)-1] of byte;cvar;external;
-        ceilingdefflags: array[0..(MAPROWS*MAPCOLS)-1] of byte;cvar;external;
-        maplights: array[0..(MAPROWS*MAPCOLS)-1] of byte;cvar;external;
-        mapsprites: array[0..(MAPROWS*MAPCOLS)-1] of byte;cvar;external;
-        mapslopes: array[0..(MAPROWS*MAPCOLS)-1] of byte;cvar;external;
-        mapeffects: array[0..(MAPROWS*MAPCOLS)-1] of byte;cvar;external;
-        mapflags: array[0..(MAPROWS*MAPCOLS)-1] of byte;cvar;external;
-        reallight: array[0..(MAPROWS*MAPCOLS)-1] of longint;cvar;external;
-        windowHeight: longint;cvar;external;
-        mapcache_height: array[0..(MAX_VIEW_HEIGHT+MAXSCROLL2)-1] of fixed_t;cvar;external;
-        frameon: longint;cvar;external;
-        CENTERY: fixed_t;cvar;external;
-        debugmode: bool;cvar;external;
+var
+  actionflag: longint;cvar;external;
+{ if set non 0, the action hook is called }
+  viewbuffer: array[0..(MAX_VIEW_WIDTH*MAX_VIEW_HEIGHT)-1] of pixel_t;cvar;external;
+  viewylookup: array[0..(MAX_VIEW_HEIGHT)-1] of ^pixel_t;cvar;external;
+  spritelump: longint;cvar;external;
+  numsprites: longint;cvar;external;
+  flattranslation: ^longint;cvar;external;
+  costable: array[0..(ANGLES+1)-1] of fixed_t;cvar;external;
+  westwall: array[0..(MAPROWS*MAPCOLS)-1] of byte;cvar;external;
+  westflags: array[0..(MAPROWS*MAPCOLS)-1] of byte;cvar;external;
+  northwall: array[0..(MAPROWS*MAPCOLS)-1] of byte;cvar;external;
+  northflags: array[0..(MAPROWS*MAPCOLS)-1] of byte;cvar;external;
+  floorpic: array[0..(MAPROWS*MAPCOLS)-1] of byte;cvar;external;
+  floorflags: array[0..(MAPROWS*MAPCOLS)-1] of byte;cvar;external;
+  ceilingpic: array[0..(MAPROWS*MAPCOLS)-1] of byte;cvar;external;
+  ceilingflags: array[0..(MAPROWS*MAPCOLS)-1] of byte;cvar;external;
+  floorheight: array[0..(MAPROWS*MAPCOLS)-1] of byte;cvar;external;
+  ceilingheight: array[0..(MAPROWS*MAPCOLS)-1] of byte;cvar;external;
+  floordef: array[0..(MAPROWS*MAPCOLS)-1] of byte;cvar;external;
+  floordefflags: array[0..(MAPROWS*MAPCOLS)-1] of byte;cvar;external;
+  ceilingdef: array[0..(MAPROWS*MAPCOLS)-1] of byte;cvar;external;
+  ceilingdefflags: array[0..(MAPROWS*MAPCOLS)-1] of byte;cvar;external;
+  maplights: array[0..(MAPROWS*MAPCOLS)-1] of byte;cvar;external;
+  mapsprites: array[0..(MAPROWS*MAPCOLS)-1] of byte;cvar;external;
+  mapslopes: array[0..(MAPROWS*MAPCOLS)-1] of byte;cvar;external;
+  mapeffects: array[0..(MAPROWS*MAPCOLS)-1] of byte;cvar;external;
+  mapflags: array[0..(MAPROWS*MAPCOLS)-1] of byte;cvar;external;
+  reallight: array[0..(MAPROWS*MAPCOLS)-1] of longint;cvar;external;
+  windowHeight: longint;cvar;external;
+  mapcache_height: array[0..(MAX_VIEW_HEIGHT+MAXSCROLL2)-1] of fixed_t;cvar;external;
+  frameon: longint;cvar;external;
+  CENTERY: fixed_t;cvar;external;
+  debugmode: bool;cvar;external;
+
     {*** FUNCTIONS **** }
 
     procedure RF_PreloadGraphics;
@@ -318,9 +316,6 @@ in define line 71 *)
     procedure RF_BlitView;
 
     procedure SetViewSize(width:longint; height:longint);
-
-{$include "protos.h"}
-{$endif}
 
 implementation
 
