04445f5297c8645e58f9261e65fa867b3eab9b2e
jval1972 <jvalavanis@gmail.com>
2020-06-14 20:27:11 +0300

    Initial convert of d_font.pas

--- Greed/d_font.pas
+++ Greed/d_font.pas
@@ -1,5 +1,7 @@
 (***************************************************************************)
 (*                                                                         *)
+(* xGreed - Source port of the game "In Pursuit of Greed"                  *)
+(* Copyright (C) 2020 by Jim Valavanis                                     *)
 (*                                                                         *)
 (* Raven 3D Engine                                                         *)
 (* Copyright (C) 1996 by Softdisk Publishing                               *)
@@ -16,333 +18,369 @@
 (*                                                                         *)
 (***************************************************************************)
 
-#include <STDARG.H>
-#include <STDIO.H>
-#include <STRING.H>
-#include 'd_global.h'
-#include 'd_video.h'
-#include 'd_misc.h'
-#include 'd_font.h'
-#include 'd_ints.h'
-#include 'r_public.h'
-#include 'protos.h'
+unit d_font;
 
+interface
 
-  const
-    MAXPRINTF = 256;    
-    MSGTIME = 350;    
-  {*** TYPES *** }
-(** unsupported pragma#pragma pack(push,packing,1)*)
+const
+  MAXPRINTF = 256;
+  MSGTIME = 350;
 
-  type
-    font_t = record
-        height : smallint;
-        width : array[0..255] of char;
-        charofs : array[0..255] of smallint;
-      end;
+type
+  font_t = packed record
+    height: smallint;
+    width: packed array[0..255] of byte;
+    charofs: packed array[0..255] of smallint;
+  end;
+  Pfont_t = ^font_t;
 
-(**** VARIABLES ****)
+const
+  MSGQUESIZE = 3;
 
-#define MSGQUESIZE 3
-
-font_t  *font;
+var
+  font: Pfont_t;
   fontbasecolor: integer;
-  fontspacing := 1: integer;
-char    str[MAXPRINTF];  // general purpose string buffer
-char    msgstr[MSGQUESIZE][MAXPRINTF];
-int     printx, printy;  // the printing position (top left corner)
-  msgtime: integer;
+  fontspacing: integer = 1;
+  printx, printy: integer;  // the printing position (top left corner)
+  timemsg: integer;
 
+procedure FN_RawPrint(const str: string);
 
-(**** FUNCTIONS ****)
+procedure FN_RawPrint2(const str: string);
 
-procedure FN_RawPrint4(char *str);
-(* Draws a string of characters to the buffer *)
-begin
-  byte b;
-  byte *dest, *source;
-  width, height, y, oldpx, yh: integer;
-  char ch;
+procedure FN_RawPrint3(const str: string);
 
-  oldpx := printx;
-  dest := viewylookup[printy]+printx;
-  height := font.height;
-  while ((ch := *str++) <> 0) do
-  begin
-   width := font.width[ch];
-   source := ((byte*)font) + font.charofs[ch];
-   while width-- do
-   begin
-     for (y := 0,yh := 0;y<height;y++,yh+:= windowWidth)
-     begin
-       b := *source++;
-       if (b) dest[yh] := fontbasecolor+b;
-       else dest[yh] := 0;
-        end;
-     dest++;
-     printx++;
-      end;
-   dest := dest + fontspacing;
-   printx := printx + fontspacing;
-    end;
-  end;
+procedure FN_RawPrint4(const str: string);
 
+function FN_RawWidth(const str: string): integer;
 
-procedure FN_RawPrint2(char *str);
-(* Draws a string of characters to the buffer *)
-begin
-  byte b;
-  byte *dest, *source;
-  width, height, y, oldpx, yh: integer;
-  char ch;
+procedure FN_Print(const s: string);
 
-  oldpx := printx;
-  dest := viewylookup[printy]+printx;
-  height := font.height;
-  while ((ch := *str++) <> 0) do
-  begin
-   width := font.width[ch];
-   source := ((byte*)font) + font.charofs[ch];
-   while width-- do
-   begin
-     for (y := 0,yh := 0;y<height;y++,yh+:= windowWidth)
-     begin
-       b := *source++;
-       if (b) dest[yh] := fontbasecolor+b;
-        end;
-     dest++;
-     printx++;
-      end;
-   dest := dest + fontspacing;
-   printx := printx + fontspacing;
-    end;
-  end;
+procedure FN_Printf(const fmt: string; const Args: array of const);
 
+procedure FN_PrintCentered(const s: string);
 
-procedure FN_RawPrint(char *str);
-(* Draws a string of characters to the screen *)
-begin
-  byte b;
-  byte *dest, *source;
-  width, height, y, oldpx, yh: integer;
-  char ch;
+procedure FN_CenterPrintf(const fmt: string; const Args: array of const);
 
-  oldpx := printx;
-  dest := ylookup[printy]+printx;
-  height := font.height;
-  while ((ch := *str++) <> 0) do
-  begin
-   width := font.width[ch];
-   source := ((byte *)font) + font.charofs[ch];
-   while width-- do
-   begin
-     for (y := 0,yh := 0;y<height;y++,yh+:= 320)
-     begin
-       b := *source++;
-       if (b) dest[yh] := fontbasecolor+b;
-       else dest[yh] := 0;
-        end;
-     dest++;
-     printx++;
-      end;
-   dest := dest + fontspacing;
-   printx := printx + fontspacing;
-    end;
-  end;
-
-
-procedure FN_RawPrint3(char *str);
-(* Draws a string of characters to the screen *)
-begin
-  byte b;
-  byte *dest, *source;
-  width, height, y, oldpx, yh: integer;
-  char ch;
-
-  oldpx := printx;
-  dest := ylookup[printy]+printx;
-  height := font.height;
-  while ((ch := *str++) <> 0) do
-  begin
-   width := font.width[ch];
-   source := ((byte *)font) + font.charofs[ch];
-   while width-- do
-   begin
-     for (y := 0,yh := 0;y<height;y++,yh+:= 320)
-     begin
-       b := *source++;
-       if (b) dest[yh] := fontbasecolor+b;
-        end;
-     dest++;
-     printx++;
-      end;
-   dest := dest + fontspacing;
-   printx := printx + fontspacing;
-    end;
-  end;
-
-
-int FN_RawWidth(char *str)
-(* Returns the width of a string
-   Does NOT handle newlines     *)
-   begin
-  width: integer;
-
-  width := 0;
-  while *str do
-  begin
-   width+:= font.width[*str++];
-   width := width + fontspacing;
-    end;
-  return width;
-  end;
-
-
-procedure FN_Print(char  *s);
-(* Prints a string in the current window, with newlines
-   going down a line and back to 0 *)
-   begin
-  char     c, *se;
-  unsigned h;
-
-  h := font.height;
-  while *s do
-  begin
-   se := s;
-   c := *se;
-   while (c) and (c <> '\n') c := *++se;
-   *se := '\0';
-   FN_RawPrint(s);
-   s := se;
-   if c then
-   begin
-     *se := c;
-     s++;
-     printx := 0;
-     printy := printy + h;
-      end;
-    end;
-  end;
-
-
-procedure FN_PrintCentered(char  *s);
-(* Prints a multi line string with each line centered *)
-begin
-  char     c, *se;
-  unsigned w, h;
-
-  h := font.height;
-  while *s do
-  begin
-   se := s;
-   c := *se;
-   while (c) and (c <> '\n') c := *++se;
-   *se := '\0';
-   w := FN_RawWidth(s);
-   printx := (320-w)/2;
-   FN_RawPrint3(s);
-   s := se;
-   if c then
-   begin
-     *se := c;
-     s++;
-     printx := 0;
-     printy := printy + h;
-      end;
-    end;
-  end;
-
-
-procedure FN_Printf(char *fmt, ...);
-(* Prints a printf style formatted string at the current print position
-    using the current print routines  *)
-    begin
-  va_list argptr;
-  cnt: integer;
-
-  va_start(argptr,fmt);
-  cnt := vsprintf(str,fmt,argptr);
-  va_end(argptr);
-{$IFDEF PARMCHECK}
-  if (cnt >= MAXPRINTF) MS_Error('FN_Printf: String too long: %s',fmt);
-{$ENDIF}
-  FN_Print(str);
-  end;
-
-
-procedure FN_CenterPrintf(char *fmt, ...);
-(* As FN_Printf, but centers each line of text in the window bounds *)
-begin
-  va_list argptr;
-  cnt: integer;
-
-  va_start(argptr,fmt);
-  cnt := vsprintf(str,fmt,argptr);
-  va_end(argptr);
-{$IFDEF PARMCHECK}
-  if (cnt >= MAXPRINTF) MS_Error('FN_CPrintf: String too long: %s',fmt);
-{$ENDIF}
-  FN_PrintCentered(str);
-  end;
-
-
-procedure FN_BlockCenterPrintf(char *fmt, ...);
-(* As FN_CenterPrintf, but also enters the entire set of lines vertically in
-   the window bounds *)
-   begin
-  va_list argptr;
-  cnt: integer;
-  char    *s;
-  height: integer;
-
-  va_start(argptr,fmt);
-  cnt := vsprintf(str,fmt,argptr);
-  va_end(argptr);
-{$IFDEF PARMCHECK}
-  if (cnt >= MAXPRINTF) MS_Error('FN_CCPrintf: String too long: %s',fmt);
-{$ENDIF}
-  height := 1;
-  s := str;
-  while (*s) if (*s++ = '\n') height++;
-  height := height * font.height;
-  printy := 0+(200-height)/2;
-  FN_PrintCentered(str);
-  end;
-
+procedure FN_BlockCenterPrintf(const fmt: string; const Args: array of const);
 
 procedure rewritemsg;
-(* write the current msg to the view buffer *)
-begin
-  i: integer;
 
+procedure writemsg(const s: string);
+
+implementation
+
+uses
+  Classes,
+  g_delphi,
+  r_render;
+
+// Draws a string of characters to the buffer
+procedure FN_RawPrint4(const str: string);
+var
+  b: byte;
+  dest: PByteArray;
+  source: PByte;
+  width, height, y, oldpx, yh: integer;
+  ch: char;
+  i: integer;
+begin
+  oldpx := printx;
+  dest := @viewylookup[printy][printx];
+  height := font.height;
+  for i := 1 to Length(str) do
+  begin
+    ch := str[i];
+    width := font.width[Ord(ch)];
+    source := @PByteArray(font)[font.charofs[Ord(ch)]];
+    while width > 0 do
+    begin
+      y := 0;
+      yh := 0;
+      while y < height do
+      begin
+        b := source^;
+        inc(source);
+        if b <> 0 then
+          dest[yh] := fontbasecolor + b
+        else
+          dest[yh] := 0;
+        inc(y);
+        yh := yh + windowWidth;
+      end;
+      dest := @dest[1];
+      inc(printx);
+      dec(witdth);
+    end;
+    dest := @dest[fontspacing];
+    printx := printx + fontspacing;
+  end;
+end;
+
+// Draws a string of characters to the buffer
+procedure FN_RawPrint2(const str: string);
+var
+  b: byte;
+  dest: PByteArray
+  source: PByte;
+  width, height, y, oldpx, yh: integer;
+  ch: char;
+  i: integer;
+begin
+  oldpx := printx;
+  dest := @viewylookup[printy][printx];
+  height := font.height;
+  for i := 1 to Length(str) do
+  begin
+    ch := str[i];
+    width := font.width[Ord(ch)];
+    source := @PByteArray(font)[font.charofs[Ord(ch)]];
+    while width > 0 do
+    begin
+      y := 0;
+      yh := 0;
+      while y < height do
+      begin
+        b := source^;
+        inc(source);
+        if b <> 0 then
+          dest[yh] := fontbasecolor + b;
+        inc(y);
+        yh := yh + windowWidth;
+      end;
+      inc(dest);
+      inc(printx);
+      dec(width);
+    end;
+    dest := @dest[fontspacing];
+    printx := printx + fontspacing;
+  end;
+end;
+
+
+// Draws a string of characters to the screen
+procedure FN_RawPrint(const str: string);
+var
+  b: byte;
+  dest: PByteArray;
+  source: PByte;
+  width, height, y, oldpx, yh: integer;
+  ch: char;
+  i: integer;
+begin
+  oldpx := printx;
+  dest := @ylookup[printy][printx];
+  height := font.height;
+  for i := 1 to Length(str) do
+  begin
+    ch := str[i];
+    width := font.width[ch];
+    source := @PByteArray(font)[font.charofs[Ord(ch)]];
+    while width > 0 do
+    begin
+      y := 0;
+      yh := 0;
+      while y < height do
+      begin
+        b := source^;
+        inc(source);
+        if b <> 0 then
+          dest[yh] := fontbasecolor + b
+        else
+          dest[yh] := 0;
+        inc(y);
+        yh := yh + 320;
+      end;
+      dest := @dest[1];
+      inc(printx);
+      dec(width);
+    end;
+    dest := @dest[fontspacing];
+    printx := printx + fontspacing;
+  end;
+end;
+
+
+// Draws a string of characters to the screen
+procedure FN_RawPrint3(const str: string);
+var
+  b: byte;
+  dest: PByteArray;
+  source: PByte;
+  width, height, y, oldpx, yh: integer;
+  ch: char;
+  i: integer;
+begin
+  oldpx := printx;
+  dest := ylookup[printy] + printx;
+  height := font.height;
+  for i := 1 to Length(str) do
+  begin
+    ch := str[i];
+    width := font.width[ch];
+    source := @PByteArray(font)[font.charofs[Ord(ch)]];
+    while width > 0 do
+    begin
+      y := 0;
+      yh := 0;
+      while y < height do
+      begin
+        b := source^;
+        inc(source);
+        if b <> 0 then
+          dest[yh] := fontbasecolor + b;
+        inc(y);
+        yh = yh + 320;
+      end;
+      inc(dest);
+      inc(printx);
+      dec(width);
+    end;
+    dest := @dest[fontspacing];
+    printx := printx + fontspacing;
+  end;
+end;
+
+
+// Returns the width of a string
+// Does NOT handle newlines
+function FN_RawWidth(const str: string): integer;
+var
+  i: integer;
+begin
+  result := 0;
+  for i := 1 to Length(str) do
+  begin
+    result := result + font.width[Ord(str[i])];
+    result := result + fontspacing;
+  end;
+end;
+
+
+// Prints a string in the current window, with newlines
+// going down a line and back to 0
+procedure FN_Print(const s: string);
+var
+  sl: TStringList;
+  i: integer;
+  stmp: string;
+  h: integer;
+begin
+  h := font.height;
+
+  sl := TStringList.Create;
+  sl.Text := s;
+
+  for i := 0 to sl.Count - 1 do
+  begin
+    stmp := sl.Strings[i];
+    if stmp <> '' then
+      FN_RawPrint(stmp);
+    printx := 0;
+    printy := printy + h;
+  end;
+end;
+
+// Prints a multi line string with each line centered
+procedure FN_PrintCentered(const s: string);
+var
+  sl: TStringList;
+  i: integer;
+  stmp: string;
+  w, h: integer;
+begin
+  h := font.height;
+
+  sl := TStringList.Create;
+  sl.Text := s;
+
+  for i := 0 to sl.Count - 1 do
+  begin
+    stmp := sl.Strings[i];
+    if stmp <> '' then
+    begin
+      w := FN_RawWidth(stmp);
+      printx := (320 - w) div 2;
+      FN_RawPrint3(stmp);
+    end;
+    printx := 0;
+    printy := printy + h;
+  end;
+end;
+
+// Prints a printf style formatted string at the current print position
+// using the current print routines
+procedure FN_Printf(const fmt: string; const Args: array of const);
+var
+  stmp: string;
+begin
+  sprintf(stmp, fmt, Args);
+  FN_Print(stmp);
+end;
+
+
+// As FN_Printf, but centers each line of text in the window bounds
+procedure FN_CenterPrintf(const fmt: string; const Args: array of const);
+var
+  stmp: string;
+begin
+  sprintf(stmp, fmt, Args);
+  FN_PrintCentered(stmp);
+end;
+
+
+// As FN_CenterPrintf, but also enters the entire set of lines vertically in
+// the window bounds
+procedure FN_BlockCenterPrintf(const fmt: string; const Args: array of const);
+var
+  stmp: string;
+  i, height: integer;
+begin
+  sprintf(stmp, fmt, Args);
+  height := font.height;
+  for i := 1 to Length(stmp) do
+    if stmp[i] = #10 then
+      inc(height, font.height);
+  printy := (200 - height) div 2;
+  FN_PrintCentered(stmp);
+end;
+
+var
+  msgstr: array[0..MSGQUESIZE - 1] of string[255];
+
+// write the current msg to the view buffer
+procedure rewritemsg;
+var
+  i: integer;
+begin
   fontbasecolor := 73;
   font := font1;
-  for(i := 0;i<MSGQUESIZE;i++)
-  if msgstr[i][0] then
-  begin
-    printx := 2;
-    printy := 1+i*6;
-    FN_RawPrint2(msgstr[i]);
-     end;
-  if timecount>msgtime then
-  begin
-   for(i := 1;i<MSGQUESIZE;i++)
-    strcpy(msgstr[i-1],msgstr[i]);
-   msgstr[MSGQUESIZE-1][0] := 0;
-   msgtime := timecount+MSGTIME;
+  for i := 0 to MSGQUESIZE - 1 do
+    if msgstr[i] <> '' then
+    begin
+      printx := 2;
+      printy := 1 + i * 6;
+      FN_RawPrint2(msgstr[i]);
     end;
+  if timecount > timemsg then
+  begin
+    for i := 1 to MSGQUESIZE - 1 do
+      msgstr[i - 1] := msgstr[i];
+    msgstr[MSGQUESIZE - 1] := '';
+    timemsg := timecount + MSGTIME;
   end;
+end;
 
-
-procedure writemsg(char *s);
-(* update current msg *)
-begin
+// update current msg
+procedure writemsg(const s: string);
+var
   i: integer;
+begin
+  if msgstr[MSGQUESIZE - 1] <> '' then
+    for i := 1 to MSGQUESIZE - 1 do
+      msgstr[i - 1] := msgstr[i];
+  msgstr[MSGQUESIZE - 1] := s;
+  timemsg := timecount + MSGTIME; // 10 secs
+end;
 
-  if msgstr[MSGQUESIZE-1][0] <> 0 then
-  for(i := 1;i<MSGQUESIZE;i++)
-   strcpy(msgstr[i-1],msgstr[i]);
-  strcpy(msgstr[MSGQUESIZE-1],s);
-  msgtime := timecount+MSGTIME; // 10 secs
-  end;
-
+end.
