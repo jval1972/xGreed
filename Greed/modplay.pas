5ff927b9830b468aa590b722d33339309c87e277
jval1972 <jvalavanis@gmail.com>
2020-06-15 23:51:16 +0300

    Initial convert of modplay.pas

--- Greed/modplay.pas
+++ Greed/modplay.pas
@@ -1,82 +1,114 @@
+(***************************************************************************)
+(*                                                                         *)
+(* xGreed - Source port of the game "In Pursuit of Greed"                  *)
+(* Copyright (C) 2020 by Jim Valavanis                                     *)
+(*                                                                         *)
+(***************************************************************************)
+(*                                                                         *)
+(* Raven 3D Engine                                                         *)
+(* Copyright (C) 1996 by Softdisk Publishing                               *)
+(*                                                                         *)
+(* Original Design:                                                        *)
+(*  John Carmack of id Software                                            *)
+(*                                                                         *)
+(* Enhancements by:                                                        *)
+(*  Robert Morgan of Channel 7............................Main Engine Code *)
+(*  Todd Lewis of Softdisk Publishing......Tools,Utilities,Special Effects *)
+(*  John Bianca of Softdisk Publishing..............Low-level Optimization *)
+(*  Carlos Hasan..........................................Music/Sound Code *)
+(*                                                                         *)
+(***************************************************************************)
+(*                                                                         *)
+(* Copyright 1996 by Robert Morgan of Channel 7                            *)
+(* Sound Interface                                                         *)
+(*                                                                         *)
+(***************************************************************************)
+
 {$I xGreed.inc}
 
-(* Copyright 1996 by Robert Morgan of Channel 7
-   Sound Interface *)
+unit modplay;
 
-#include <STDIO.H>
-#include <STDLIB.H>
-#include <STRING.H>
-#include <IO.H>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include 'd_global.h'
-#include 'd_misc.h'
-#include 'd_disk.h'
-#include 'd_video.h'
-#include 'protos.h'
-#include 'r_refdef.h'
-#include 'd_ints.h'
+interface
 
+uses
+  protos_h,
+  r_public_h;
 
-(**** CONSTANTS ****)
+const
+  MAXEFFECTS = 8;
+//  NUMTRACKS = effecttracks;
+  MAXCACHESIZE = MAXEFFECTS * 2 + 4;
+  MAXSOUNDS = 90;
+  MAXSOUNDDIST = 384;
+  MSD = MAXSOUNDDIST shl FRACBITS;
+  DEFAULTVRDIST = 157286;
+  DEFAULTVRANGLE = 4;
 
-#define MAXEFFECTS    8
-#define NUMTRACKS     effecttracks
-#define MAXCACHESIZE  (MAXEFFECTS*2+4)
-#define MAXSOUNDS     90
-#define MAXSOUNDDIST  384
-#define MSD           (MAXSOUNDDIST shl FRACBITS)
-#define DEFAULTVRDIST  157286
-#define DEFAULTVRANGLE 4
-
-(**** VARIABLES ****)
-
+var
   MusicPresent, MusicPlaying, MusicSwapChannels: boolean;
-SoundCard SC;
+  SC: SoundCard;
   MusicError, EffectChan, CurrentChan, FXLump: integer;
   MusicVol, effecttracks: integer;
 
+implementation
 
-(**** FUNCTIONS ****)
+uses
+  g_delphi,
+  d_ints_h,
+  raven;
 
-int LoadSetup(SoundCard *SC, char *Filename)
+function LoadSetup(const SC: PSoundCard; const Filename: string): integer;
+var
+  f: file;
 begin
-  Handle: integer;
-    if ((Handle :=  _open(Filename,O_RDONLY) or (O_BINARY)) < 0) then
-        return 1;
-    if (read(Handle,SC,SizeOf(SoundCard)) <> SizeOf(SoundCard))  begin 
-        close(Handle);
-        return 1;
-     end;
-    close(Handle);
-    return 0;
+  if not fopen(f, FileName, fOpenReadOnly) then
+  begin
+    result := 1;
+    exit;
+  end;
+  if not fread(SC, SizeOf(SoundCard), 1, f) then
+  begin
+    close(f);
+    result := 1;
+    exit;
   end;
 
+  close(f);
+  result := 0;
+end;
 
-int SaveSetup(SoundCard *SC, char *Filename)
+
+function SaveSetup(const SC: PSoundCard; const Filename: string): integer;
+var
+  f: file;
 begin
-  Handle: integer;
-    if ((Handle :=  fopen(Filename, 'w')) < 0) then
-        return 1;
-    if (write(Handle,SC,SizeOf(SoundCard)) <> SizeOf(SoundCard))  begin 
-        close(Handle);
-        return 1;
-     end;
-    close(Handle);
-    return 0;
+  if not fopen(f, FileName, fOpenReadWrite) then
+  begin
+    result := 1;
+    exit;
   end;
 
+  if not fwrite(SC, SizeOf(SoundCard), 1, f) then
+  begin
+    close(f);
+    result := 1;
+    exit;
+  end;
+
+  close(f);
+  result := 0;
+end;
 
 procedure StopMusic;
-begin
+var
   i: integer;
-
-  if MusicError then
-  exit;
+begin
+  if MusicError <> 0 then
+    exit;
   if MusicPlaying then
   begin
-   if not netmode then
-    for(i := MusicVol;i>0;i-:= 3)              (* fade out *)
+{    if not netmode then
+      for i := MusicVol i>0;i-:= 3)              (* fade out *)
     begin
       if (i<0) i := 0;
       //dSetMusicVolume(i);
@@ -84,85 +116,88 @@ begin
        end;
    //dStopMusic;
    MusicPlaying := false;
-   //dFreeModule(M);
-    end;
-  if netmode then
-  NetGetData;
+   //dFreeModule(M);}
   end;
+  if netmode then
+    NetGetData;
+end;
 
 
 procedure InitSound;
-begin
+var
   autodetect, noconfig: boolean;
-
+begin
   MusicPresent := false;
   autodetect := false;
   noconfig := false;
-  if (LoadSetup and (SC,'SETUP.CFG'))     (* load config file *) then
+
+  // load config file
+  if LoadSetup(@SC, 'SETUP.CFG') <> 0 then
   begin
-   noconfig := true;
-   printf('Sound:\tSETUP.CFG not found\n');
-   printf('\tAuto-Detection := ');
-   autodetect := false;
+    noconfig := true;
+    printf('Sound: SETUP.CFG not found'#13#10);
+    printf('       Auto-Detection = ');
+    autodetect := false;
 
-   SC.ambientlight := 2048;      // load all defaults
-   SC.violence := true;
-   SC.animation := true;
-   SC.musicvol := 100;
-   SC.sfxvol := 128;
-   SC.ckeys[0] := scanbuttons[bt_run];
-   SC.ckeys[1] := scanbuttons[bt_jump];
-   SC.ckeys[2] := scanbuttons[bt_straf];
-   SC.ckeys[3] := scanbuttons[bt_fire];
-   SC.ckeys[4] := scanbuttons[bt_use];
-   SC.ckeys[5] := scanbuttons[bt_useitem];
-   SC.ckeys[6] := scanbuttons[bt_asscam];
-   SC.ckeys[7] := scanbuttons[bt_lookup];
-   SC.ckeys[8] := scanbuttons[bt_lookdown];
-   SC.ckeys[9] := scanbuttons[bt_centerview];
-   SC.ckeys[10] := scanbuttons[bt_slideleft];
-   SC.ckeys[11] := scanbuttons[bt_slideright];
-   SC.ckeys[12] := scanbuttons[bt_invleft];
-   SC.ckeys[13] := scanbuttons[bt_invright];
-   SC.inversepan := false;
-   SC.screensize := 0;
-   SC.camdelay := 35;
-   SC.effecttracks := 4;
-   SC.mouse := 1;
-   SC.joystick := 0;
+    SC.ambientlight := 2048;      // load all defaults
+    SC.violence := true;
+    SC.animation := true;
+    SC.musicvol := 100;
+    SC.sfxvol := 128;
+    SC.ckeys[0] := scanbuttons[bt_run];
+    SC.ckeys[1] := scanbuttons[bt_jump];
+    SC.ckeys[2] := scanbuttons[bt_straf];
+    SC.ckeys[3] := scanbuttons[bt_fire];
+    SC.ckeys[4] := scanbuttons[bt_use];
+    SC.ckeys[5] := scanbuttons[bt_useitem];
+    SC.ckeys[6] := scanbuttons[bt_asscam];
+    SC.ckeys[7] := scanbuttons[bt_lookup];
+    SC.ckeys[8] := scanbuttons[bt_lookdown];
+    SC.ckeys[9] := scanbuttons[bt_centerview];
+    SC.ckeys[10] := scanbuttons[bt_slideleft];
+    SC.ckeys[11] := scanbuttons[bt_slideright];
+    SC.ckeys[12] := scanbuttons[bt_invleft];
+    SC.ckeys[13] := scanbuttons[bt_invright];
+    SC.inversepan := false;
+    SC.screensize := 0;
+    SC.camdelay := 35;
+    SC.effecttracks := 4;
+    SC.mouse := 1;
+    SC.joystick := 0;
 
-   SC.chartype := 0;
-   SC.socket := 1234;
-   SC.numplayers := 2;
-   SC.serplayers := 1;
-   SC.com := 1;
-   SC.rightbutton := bt_north;
-   SC.leftbutton := bt_fire;
-   SC.joybut1 := bt_fire;
-   SC.joybut2 := bt_straf;
-   strncpy(SC.dialnum,'           ',12);
-   strncpy(SC.netname,'           ',12);
-   SC.netmap := 22;
-   SC.netdifficulty := 2;
-   SC.mousesensitivity := 32;
-   SC.turnspeed := 8;
-   SC.turnaccel := 2;
+    SC.chartype := 0;
+    SC.socket := 1234;
+    SC.numplayers := 2;
+    SC.serplayers := 1;
+    SC.com := 1;
+    SC.rightbutton := bt_north;
+    SC.leftbutton := bt_fire;
+    SC.joybut1 := bt_fire;
+    SC.joybut2 := bt_straf;
+    strncpy(SC.dialnum, '           ',12);
+    strncpy(SC.netname, '           ',12);
+    SC.netmap := 22;
+    SC.netdifficulty := 2;
+    SC.mousesensitivity := 32;
+    SC.turnspeed := 8;
+    SC.turnaccel := 2;
 
-   SC.vrhelmet := 0;
-   SC.vrangle := DEFAULTVRANGLE;
-   SC.vrdist := DEFAULTVRDIST;
+    SC.vrhelmet := 0;
+    SC.vrangle := DEFAULTVRANGLE;
+    SC.vrdist := DEFAULTVRDIST;
 
-   lighting := 1;
-   changelight := SC.ambientlight;
+    lighting := 1;
+    changelight := SC.ambientlight;
 
-   if autodetect then
-   begin
-     printf('Failed\n');
-     MusicError := 1;
-     exit;
-      end;
-   else printf('Success\n');
-    end;
+    if autodetect then
+    begin
+      printf('Failed'#13#10);
+      MusicError := 1;
+      exit;
+    end
+    else
+      printf('Success'#13#10);
+  end;
 
   MusicSwapChannels := SC.inversepan;
 
@@ -189,38 +224,40 @@ begin
   effecttracks := SC.effecttracks;
 
   MusicError := 2;
-  exit;
-  end;
+end;
 
-procedure PlaySong(char *sname,int pattern);
+procedure PlaySong(const sname: string; const pattern: integer);
 begin
-  end;
+end;
 
 
-procedure SoundEffect(int n,int variation,fixed_t x,fixed_t y);
+procedure SoundEffect(const n: integer; const variation: integer; const x, y: fixed_t);
 begin
-  end;
+end;
 
-
-procedure StaticSoundEffect(int n,fixed_t x,fixed_t y);
+procedure StaticSoundEffect(const n: integer; const x, y: fixed_t);
 begin
-  end;
+end;
 
 
 procedure UpdateSound;
 begin
-  end;
+end;
 
 
-procedure SetVolumes(int music,int fx);
+procedure SetVolumes(const music: integer; const fx: integer);
 begin
-  if MusicError then
-  exit;
-  if (music>255) music := 255;
+  if MusicError <> 0 then
+    exit;
+  if music > 255 then
+    music := 255;
   //dSetMusicVolume(music);
   MusicVol := music;
-  if (fx>255) fx := 255;
+  if fx > 255 then
+    fx := 255;
   //dSetSoundVolume(fx);
   SC.musicvol := music;
   SC.sfxvol := fx;
-  end;
+end;
+
+end.
